e75438c68d4d83d46e318f301b9a48fb75b24028
Pynchon90 <igorbel13@gmail.com>
2022-10-13 22:08:32 +0300

    Lesson 8. Number and String Validation

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const domInpTodoTitle = document.getElementById("inpTodoTitle");
 const domBtnCreateTodo = document.getElementById("btnCreateTodo");
 const domListOfTodos = document.getElementById("listOfTodos");
 
+domBtnCreateTodo.addEventListener("click", onBtnCreateTodoClick)
+
 class TodoVO {
     constructor(id, title, date = new Date()) {
         this.id = id;
@@ -13,24 +15,47 @@ class TodoVO {
 
 const listOfTodos = [];
 
-domInpTodoTitle.value = "Todo text";
-
-domBtnCreateTodo.addEventListener("click", (e) => {
-    console.log("> domBtnCreateTodo -> handle(click)", e);
+function onBtnCreateTodoClick(event) {
+    console.log("> domBtnCreateTodo -> handle(click)", event);
     const todoTitleValueFromDomInput = domInpTodoTitle.value;
-    console.log(
-        "> domBtnCreateTodo -> todoInputTitleValue:",
-        todoTitleValueFromDomInput
-    );
+    console.log("> domBtnCreateTodo -> todoInputTitleValue:", todoTitleValueFromDomInput);
 
-    const todoId = Date.now().toString();
-    const todoVO = new TodoVO(todoId, todoTitleValueFromDomInput);
+    //const isInputValueString = typeof todoTitleValueFromDomInput === 'string';
+   //const isInputValueNotNumber = isNaN(parseInt(todoTitleValueFromDomInput))    ;
+   const canCreateTodo =  validateTodoInputTitleValue(todoTitleValueFromDomInput);
+    //    && isInputValueNotNumber
+     //   && todoTitleValueFromDomInput.length > 0;
 
-    listOfTodos.push(todoVO);
-
-    let output = "";
-    for (let index in listOfTodos) {
-        output += `<li>${listOfTodos[index].title}</li>`;
+    if(canCreateTodo){
+        //const todoVO = createTodoVO(todoTitleValueFromDomInput)
+        const todoId = Date.now().toString();
+        const todoVO = new TodoVO(todoId, todoTitleValueFromDomInput);
+        listOfTodos.push(todoVO);
+        let output = "";
+        for (let index in listOfTodos) {
+            output += `<li>${listOfTodos[index].title}</li>`;
+        }
+        domListOfTodos.innerHTML = output;
     }
-    domListOfTodos.innerHTML = output;
-});
+}
+
+function validateTodoInputTitleValue(value){
+    const isInputValueString = typeof value === 'string';
+    const isInputValueNotNumber = isNaN(parseInt(value))    ;
+    const result =
+        isInputValueString
+        && isInputValueNotNumber
+        && value.length > 0;
+    console.log('> validateTodoInputTitleValue -> result',{
+        result,
+        isInputValueString,
+        isInputValueNotNumber
+    });
+    return result
+}
+
+function createTodoVO(title) {
+    const todoId = Date.now().toString();
+    //const todoVO = new TodoVO(todoId, title);
+    return  new TodoVO(todoId, title);
+}
