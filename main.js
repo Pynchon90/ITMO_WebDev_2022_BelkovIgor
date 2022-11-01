317cacd0b4ee98f2af2690f2a3b2e1c2bb9d6d91
Pynchon90 <igorbel13@gmail.com>
2022-11-01 12:54:16 +0300

    Planets OOP

--- main.js
+++ main.js
@@ -1,121 +1,80 @@
-import TodoVO from './src/model/VOS/TodoVO.js';
-import { disableButtonWhenTextInvalid } from './src/utils/domUtils.js';
-import { isStringNotNumberAndNotEmpty } from './src/utils/stringUtils.js';
-import { localStorageListOf, localStorageSaveListOfWithKey } from './src/utils/databaseUtils.js';
-import TodoView from './src/view/TodoView.js';
+const canvas = document.createElement('canvas');
 
-const domInpTodoTitle = document.getElementById('inpTodoTitle');
-const domBtnCreateTodo = document.getElementById('btnCreateTodo');
-const domListOfTodos = document.getElementById('listOfTodos');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-let selectedTodoVO = null;
-let selectedTodoViewItem = null;
+canvas.style.backgroundColor = '#f1f1f1';
 
-domBtnCreateTodo.addEventListener('click', onBtnCreateTodoClick);
-domBtnCreateTodo.addEventListener('keyup', onInpTodoTitleKeyup);
-domListOfTodos.addEventListener('change', onTodoListChange);
-domListOfTodos.addEventListener('click', onTodoDomItemClicked);
+document.getElementById('app').append(canvas);
 
-const LOCAL_LIST_OF_TODOS = 'listOfTodos';
-const LOCAL_INPUT_TEXT = 'inputText';
+const ctx = canvas.getContext('2d');
 
-const listOfTodos = localStorageListOf(LOCAL_LIST_OF_TODOS);
-
-console.log('> Initial value -> listOfTodos', listOfTodos);
-domInpTodoTitle.value = localStorage.getItem(LOCAL_INPUT_TEXT);
-renderTodoListInContainer(listOfTodos, domListOfTodos);
-disableOrEnableCreateTodoButtonOnTodoInputTitle();
-
-function onTodoDomItemClicked(event) {
-  const domElement = event.target;
-  const target = event.target;
-  console.log('> onTodoDomItemClicked click -> dataset:', target.dataset);
-  if (domElement.dataset['type'] !== TodoView.TODO_VIEW_ITEM) return;
-
-  const SELECTED_ITEM_BACKGROUND_KEY = 'lightgray';
-
-  function validateTodoItemById(vo) {
-    return vo.id === todoId;
-  }
-  const todoId = target.id;
-  const todoVO = listOfTodos.find(validateTodoItemById);
-
-  const isSelected = target.style.backgroundColor === SELECTED_ITEM_BACKGROUND_KEY;
-
-  if (isSelected) {
-    target.style.backgroundColor = '';
-  } else {
-    target.style.backgroundColor = SELECTED_ITEM_BACKGROUND_KEY;
+class Position {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
   }
 }
 
-function onTodoListChange(event) {
-  console.log('> onTodoListChange -> event', event);
-  const target = event.target;
-  const index = target.id;
-  if (index) {
-    const indexInt = parseInt(index.trim());
-    const todoVO = listOfTodos[indexInt];
-    console.log('> onTodoListChange -> todoVO', indexInt, todoVO);
-    todoVO.isCompleted = !todoVO.isCompleted;
-    saveListOfTodo();
+class Planet {
+  constructor(color, atmosphere, position, size) {
+    this.color = color;
+    this.atmosphere = atmosphere;
+    this.position = position;
+    this.size = size;
+  }
+  render(ctx) {
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+    ctx.strokeStyle = this.atmosphere;
+    ctx.lineWidth = 10;
+    ctx.arc(this.position.x, this.position.y, this.size, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.fill();
   }
 }
 
-function onBtnCreateTodoClick() {
-  const todoTitleValueFromDomInput = domInpTodoTitle.value;
-
-  if (isStringNotNumberAndNotEmpty(todoTitleValueFromDomInput)) {
-    //if (selectedTodoVO){
-    //  selectedTodoVO.title = todoTitleValueFromDomInput;
-    //  resetSelectedTodo();
-    //}
-    //else{
-    //  createTodoFromTextAndAddToListThenSave(todoTitleValueFromDomInput, listOfTodos);
-    //  clearInputTextAndLocalStorage();
-    //}
-    createTodoFromTextAndAddToListThenSave(todoTitleValueFromDomInput);
-    saveListOfTodo();
-    setInputTextValueAndStoreToLocalStorage();
-    renderTodoListInContainer(listOfTodos, domListOfTodos);
-    disableOrEnableCreateTodoButtonOnTodoInputTitle();
+class Earth extends Planet {
+  constructor(position) {
+    super('green', 'blue', position, 40);
+  }
+  render(ctx) {
+    super.render(ctx);
+    ctx.fillStyle = 'blue';
+    ctx.beginPath();
+    ctx.arc(this.position.x, this.position.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
   }
 }
-//function validateTodoInputTitleValue(value) {
-//  const isInputValueString = typeof value === 'string';
-//  const isInputValeNotNumber = isNaN(parseInt(value));
-//}
-
-function onInpTodoTitleKeyup(event) {
-  console.log('> onInpTodoTitleKeyup', event);
-  const inputValue = event.currentTarget.value;
-  console.log('> inputValue', inputValue);
-  disableOrEnableCreateTodoButtonOnTodoInputTitle();
-  localStorage.setItem(LOCAL_INPUT_TEXT, inputValue);
-}
-
-function renderTodoListInContainer(listOfTodoVO, container) {
-  let output = '';
-  let todoVO;
-  for (let index in listOfTodoVO) {
-    todoVO = listOfTodoVO[index];
-    output += TodoView.createSimpleViewFromVO(index, todoVO);
+class Sun extends Planet {
+  constructor(position) {
+    super('red', 'yellow', position, 100);
   }
-  container.innerHTML = output;
 }
 
-function createTodoFromTextAndAddToListThenSave(input) {
-  listOfTodos.push(TodoVO.createFromTitle(input));
-}
-function setInputTextValueAndStoreToLocalStorage(clearToText = '') {
-  domInpTodoTitle.value = '';
-  localStorage.setItem(LOCAL_INPUT_TEXT, clearToText);
-}
+const sun = new Sun(new Position(canvas.width / 2, canvas.height / 2));
+const earthPositionRelativeToSun = getPositionRelativeToPlanetWithOffset(sun, 10, 40);
+const earth = new Earth(earthPositionRelativeToSun);
+earth.render(ctx);
+sun.render(ctx);
 
-function disableOrEnableCreateTodoButtonOnTodoInputTitle() {
-  disableButtonWhenTextInvalid(domBtnCreateTodo, domInpTodoTitle.value, isStringNotNumberAndNotEmpty);
-}
+let alpha = 0;
+//window.requestAnimationFrame(renderPlanets)
 
-function saveListOfTodo() {
-  localStorageSaveListOfWithKey(LOCAL_LIST_OF_TODOS, listOfTodos);
+setInterval(() => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  //const offsetPosition = getPositionRelativeToPlanetWithOffset(sun, 10, earth.size);
+  earth.position.x = 200 * Math.sin(alpha) + sun.position.x;
+  earth.position.y = 200 * Math.cos(alpha) + sun.position.y;
+  earth.render(ctx);
+  sun.render(ctx);
+  alpha += 1 / Math.PI;
+  if (alpha >= 2 * Math.PI) alpha = 0;
+}, 100);
+
+function getPositionRelativeToPlanetWithOffset(planet, offset, radius) {
+  return new Position(
+    planet.position.x + planet.size + offset + radius,
+    planet.position.y + planet.size + offset + radius
+  );
 }
