16ae51ae5aca11070ce10e56dae50a93630ebe27
Pynchon90 <igorbel13@gmail.com>
2022-12-15 20:58:33 +0300

    Express with static website

--- index.js
+++ index.js
@@ -1,48 +1,56 @@
-// console.log('Welcome Node');
-// console.log(process);
-//
+// console.log("Welcome Node");
+
 // let count = 0;
-//
-// const func = ()=>{
-//     console.log(`Counter ${++count}`);
-//     if (count > 10) clearInterval(intervalId);
-// };
-//
+
+// const func = () => {
+//   console.log(`Counter ${++count}`);
+//   if (count > 3) clearInterval(intervalId);
+// }
+
 // const intervalId = setInterval(func, 1000);
 
 require('dotenv').config();
-const express = require('express')
+
+const express = require('express');
 const fs = require('node:fs/promises');
-const app = express()
+const app = express();
 
-process.on("exit", ()=>{
+process.on("exit", () => {
     console.log("Goodbye Node");
-});
+})
 
-const users = [];
+app.use('/site', express.static('public'))
 
 app.get('/', (req, res) => {
     const { name } = req.query;
     res.send(`
     Welcome ${name} to users application!
     ${JSON.stringify(db.users)}
-    <form action="/users" method="post" class="form-example">
-  <div class="form-example">
-    <label for="name">Enter your name: </label>
-    <input type="text" name="name" id="name" required>
-  </div>
-  <div class="form-example">
-    <input type="submit" value="Submit!">
-  </div>
-</form>
+    <form  action="/users" method="post">
+      <div class="form-example">
+        <label for="name">Enter your name: </label>
+        <input id="btnSubmit" type="text" name="name" id="name" required>
+        <label for="surname">Enter your name: </label>
+        <input type="text" name="surname" id="surname" required>
+      </div>
+      <div class="form-example">
+        <input type="submit" value="Submit!">
+      </div>
+      
+    </form>
+    <script>
+      document.getElementById('btnSubmit').onkeyup = () => console.log("Clicked");
+    </script>
   `);
 });
 
 app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded());
+
 app.get('/users', (req, res) => {
     res.status(200).json(db.users);
 });
+
 app.post('/users', async (req, res) => {
     const userData = req.body;
     console.log(userData);
@@ -50,6 +58,7 @@ app.post('/users', async (req, res) => {
     await fs.writeFile('./db.json', JSON.stringify(db));
     res.status(200).json(db.users);
 });
+
 app.get('/users/:id', (req, res) => {
     const userId = parseInt(req.params.id);
     console.log('> userId', userId);
@@ -58,6 +67,7 @@ app.get('/users/:id', (req, res) => {
     else res.status(500).json({ message: `User with id(${userId}) not found` });
 });
 
+// to support URL-encoded bodies
 app.use((req, res, next) => {
     res.status(404).send(
         `
@@ -65,11 +75,11 @@ app.use((req, res, next) => {
       <a href="/">Home</a>
       `
     )
-})
+});
 
 let db;
 
-app.listen(process.env.PORT || 3000, async() => {
+app.listen(process.env.PORT || 3000, async () => {
     db = JSON.parse(await fs.readFile('./db.json', 'utf8'));
     console.log(`Example app listening on port ${process.env.PORT}`, db)
 });
