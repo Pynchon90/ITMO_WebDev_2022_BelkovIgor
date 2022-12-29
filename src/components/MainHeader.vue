18c8eab2813cdff6babc69ed767a3ffc94daeb5c
Pynchon90 <igorbel13@gmail.com>
2022-12-29 18:56:01 +0300

    Pocketbase sqlite3

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { computed } from "vue";
+import Routes from '../consts/Routes';
+const props = defineProps<{ userData: any }>();
+const isUserAuthenticated = computed(()=> !!props.userData);
+</script>
+
+<template>
+  <header>
+ <div>Header</div>
+  <div v-if="isUserAuthenticated">{{ userData.username}}</div>
+  <div v-else>
+    <RouterLink :to="Routes.LOGIN">Login</RouterLink>
+  </div>
+  </header>
+</template>
+<script lang="ts">
+export default {
+  computed: {},
+  methods: {},
+};
+</script>
+
+<style scoped>
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { createApp } from 'vue'
 import './style.css'
+import PocketBase from 'pocketbase'
 import App from './App.vue'
+import {createRouter, createWebHashHistory} from "vue-router";
+import Routes from "./consts/Routes";
+import router from './router'
+import { createLogger } from "vite";
+// import BooksListPage from './pages/BooksListPage.vue'
 
-//const db = new PouchDB('http://localhost:5984/books', {skip_setup: true});
+const pb = new PocketBase(import.meta.env.VITE_DATABASE_URL);
+console.log('> pb.authStore.isValid: ', pb.authStore.isValid);
+router.beforeEach((to, from, next) =>{
+  const authRoutes = [Routes.INDEX, Routes.LOGIN, Routes.REGISTER];
+  const indexOfAuthRoute = authRoutes.indexOf(to.path);
+  if (indexOfAuthRoute <0 && !pb.authStore.isValid) {
+    next ({path: Routes.LOGIN})
+  }
+  else next();
+});
+
+
+
 const app = createApp(App);
-//app.provide('db', db);
+app.use(router);
+app.provide('pb', pb);
 app.mount('#app');
