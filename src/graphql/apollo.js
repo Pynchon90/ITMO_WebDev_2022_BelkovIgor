fe42eb4cb431f6ba40e622e0a46e9545b6ce844e
Pynchon90 <igorbel13@gmail.com>
2023-01-17 18:51:13 +0300

    Lesson-42. Preparation to Hasura Integration

---
+++ src/graphql/apollo.js
@@ -0,0 +1,26 @@
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+} from '@apollo/client/core';
+
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: 'https://welcome-pony-27.hasura.app/v1/graphql',
+  headers: {
+    'x-hasura-admin-secret':
+      '2ER7DPVz2aJuWuGDgztj3tH8565Hvxo7sveRYAzWKifMdGOsLljGHRflBvZeHwCQ',
+  },
+});
+
+// Cache implementation
+const cache = new InMemoryCache();
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+});
+
+export default apolloClient;
--- src/pages/BooksPage.vue
+++ src/pages/BooksPage.vue
@@ -1,12 +1,22 @@
 <script setup lang="ts">
-import { inject, ref, computed, onMounted, onUnmounted } from 'vue';
+import { inject, ref, computed, onMounted, onUnmounted, watch } from 'vue';
+import { useLazyQuery } from '@vue/apollo-composable';
+import UploadBooks from '../components/UploadBooks.vue';
+import gql from 'graphql-tag';
 
 const LIMIT = 10;
 
-const pb: any = inject('pb');
+const { result, load, fetchMore } = useLazyQuery(gql`
+  query GetBooks {
+    books {
+      title
+    }
+  }
+`);
 
-const domInputFile = ref(null);
-const booksList = ref([]);
+// const pb: any = inject('pb');
+
+const booksList = ref<any>([]);
 const currentPageIndex = ref(0);
 const maxBooks = ref(0);
 const isLoading = ref(true);
@@ -22,15 +32,13 @@ const canRenderNextPageButton = computed(
 const loadBooks = async () =>
   Promise.resolve()
     .then(() => (isLoading.value = true))
-    .then(() => pb
-      .collection('books')
-      .getList(1, LIMIT, {})
-    ).then((result) => {
+    .then(() => pb.collection('books').getList(1, LIMIT, {}))
+    .then((result) => {
       booksList.value = result.items;
       isLoading.value = false;
       maxBooks.value = result.totalItems;
       console.log(canRenderUpload.value, maxBooks.value, result);
-  });
+    });
 
 const getBookIndex = (index: number) =>
   1 + index + currentPageIndex.value * LIMIT;
@@ -45,49 +53,41 @@ const onPagePrevClick = () => {
   loadBooks();
 };
 
-const onUploadClick = () => {
-  console.log('> onUploadClick');
-  const input = domInputFile.value! as HTMLInputElement;
-  input.onchange = () => {
-    const fileList = input.files as FileList;
-    const selectedFile = fileList[0];
-    console.log('selectedFile:', selectedFile);
-    const reader = new FileReader();
-    reader.onload = async () => {
-      const books = JSON.parse(reader.result! as string);
-      console.log('selectedFile:', books);
-      for (let book of books) {
-        console.log('> book', book);
-        await pb.collection('books').create(book);
-      }
-      reader.onload = null;
-    };
-    reader.readAsText(selectedFile);
-    input.onchange = null;
-  };
-  input.click();
-};
+// pb.collection('books').subscribe('*', (change: any) => {
+//   console.log('> change', change);
+//   loadBooks();
+// });
 
-pb.collection('books')
-  .subscribe('*',(change: any) => {
-    console.log('> change', change);
-    loadBooks();
+const onUploadComplete = () => {
+  console.log('> BooksPage -> onUploadComplete:');
+  fetchMore({
+    updateQuery: (previousResult, { fetchMoreResult }) => {
+      if (!fetchMoreResult) return;
+      isLoading.value = false;
+      maxBooks.value = fetchMoreResult.books.length;
+      booksList.value = fetchMoreResult.books;
+    },
   });
+};
 
 onMounted(() => {
   console.log('> BooksPage -> onMounted');
-  loadBooks();
+  load();
+  const unwatch = watch(result, (value) => {
+    unwatch();
+    isLoading.value = false;
+    maxBooks.value = value.books.length;
+    booksList.value = value.books;
+    console.log(value);
+  });
 });
 onUnmounted(() => {
-  pb.collection('books').unsubscribe();
+  // pb.collection('books').unsubscribe();
 });
 </script>
 
 <template>
-  <div v-if="canRenderUpload">
-    <input hidden ref="domInputFile" type="file" />
-    <button @click="onUploadClick">Upload</button>
-  </div>
+  <UploadBooks v-if="canRenderUpload" @upload-complete="onUploadComplete" />
   <div v-else-if="isLoading">Loading...</div>
   <div v-else>
     <h2>
